90ccc6787558caf13698dafe65bed0e1906b282e
Gassan <g_fox_fts@yahoo.com>
2020-07-23 12:38:04 +0200

    if

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
+
+console.log(1 == '1')
+console.log([])
+console.log(!!-3.14)
+const condition=false
+if(condition){
+    console.log('true')
+}else{
+    console.log('false')
+}
+const number =99
+let result
+if(number === 1){
+    result='one'
+}else{
+    result ='No Match'
+}
+console.log(result)
