ad4efebeb09077272f6e2a889ea1ff9173605745
Gassan <g_fox_fts@yahoo.com>
2020-07-23 12:54:45 +0200

    ? :

--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
-console.log(1 == '1')
-console.log([])
-console.log(!!-3.14)
-const condition=false
-if(condition){
-    console.log('true')
-}else{
-    console.log('false')
-}
-const number =99
-let result
-if(number === 1){
-    result='one'
-}else{
-    result ='No Match'
-}
+const number=10
+//const anotherNumber=1
+//const result ='The number is ' + (number ===1?'one':'no Match')
+const result =`The number is  ${(number ===1
+    ?'one'
+    : number===99
+    ? 'ninty-nine'
+    :number)}
+    `
+
 console.log(result)
