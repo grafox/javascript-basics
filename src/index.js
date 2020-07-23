40fd62b36d957aa6aac2fa0d1707fa3cbf3a2c2e
Gassan <g_fox_fts@yahoo.com>
2020-07-23 13:19:39 +0200

    switch

--- src/index.js
+++ src/index.js
@@ -3,14 +3,36 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
-const number=10
+//const number=10
+//let result
 //const anotherNumber=1
 //const result ='The number is ' + (number ===1?'one':'no Match')
+/* 
 const result =`The number is  ${(number ===1
     ?'one'
     : number===99
     ? 'ninty-nine'
     :number)}
     `
+console.log(result) */
+/* switch(number){
+    case 1:result='one'; break
+    case 99:result= 'Ninty-nine'; break
+    default: result='No Match'
+ }
 
-console.log(result)
+ switch(number){
+    case 1:{
+        const h='hi'
+        result='one'+h; 
+        break
+    }
+    case 99:
+        result= 'Ninty-nine'; 
+        break
+
+        default: result='No Match'
+ }
+ console.log(result)
+//constructor
+ console.log(new Number(66)) */
